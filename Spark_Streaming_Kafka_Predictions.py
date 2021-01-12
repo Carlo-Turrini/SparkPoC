73047438f9ec000c0e212efc2fb7d9ef267adf46
Carlo Turrini <carlo.turrini@student.unife.it>
2021-01-12 23:17:38 +0100

    Little modifications

--- Spark_Streaming_Kafka_Predictions.py
+++ Spark_Streaming_Kafka_Predictions.py
@@ -17,7 +17,7 @@ from seldon_core.seldon_client import SeldonClient
 spark-submit \
 --jars /mnt/c/Users/carlo/Desktop/SbtScalaDemo/target/scala-2.12/sbtscalademo_2.12-0.2.jar \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 \
-Streaming_Demo.py
+Spark_Streaming_Kafka_Predictions.py
 """
 def main():
     ss = None
